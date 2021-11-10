1b97bd2eedc67adef53e28e591f7d788680f214a
Artem Krasov <artkrasov@gmail.com>
2021-11-10 20:09:01 +0300

    Ensured the preservation of information between work sessions

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.room.*
+import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,6 +17,30 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import java.util.*
+
+@Entity
+data class NewtonRoom(
+    @ColumnInfo(name = "result") val result: String,
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
+
+@Dao
+interface NewtonDao {
+    @Query("SELECT * FROM NewtonRoom")
+    fun getAll(): NewtonRoom
+    @Insert
+    fun insertAll(vararg newtonResult: NewtonRoom)
+    @Query("DELETE FROM newtonRoom")
+    fun dropAll()
+}
+
+@Database(entities = [NewtonRoom::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun newtonDao(): NewtonDao
+}
 
 data class Newton (
     var result: String? = null
@@ -22,9 +48,11 @@ data class Newton (
 
 private interface APINewton {
     @GET("{operation}/{expression}")
-    fun getNewton(@Path("operation") operation : String, @Path("expression") expression : String) : Call<Newton>
+    fun getNewton(@Path("operation") operation : String,
+                  @Path("expression") expression : String) : Call<Newton>
 }
 
+@DelicateCoroutinesApi
 class MainActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -33,45 +61,69 @@ class MainActivity : AppCompatActivity() {
         outState.putString(RESULT, textView.text.toString())
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val retrofit = Retrofit.Builder().baseUrl("https://newton.now.sh/api/v2/").
-                        addConverterFactory(GsonConverterFactory.create()).build()
+        val retrofit = Retrofit.Builder().baseUrl("https://newton.now.sh/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
 
         val publicNewton = retrofit.create(APINewton::class.java)
 
-        val textEditOperation = findViewById<EditText>(R.id.operation)
-        val textEditExpression = findViewById<EditText>(R.id.expression)
-        val textViewResult = findViewById<TextView>(R.id.textViewResult)
+        val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "newton").build()
+        val newtonDao = db.newtonDao()
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val adapter = NewtonRoom(newtonDao.getAll().result)
+            launch(Dispatchers.Main) {
+                findViewById<TextView>(R.id.textViewResult).text =
+                    getString(R.string.resultTextView) + " "+ adapter.result
+            }
+        }
 
         if (savedInstanceState != null)
-            textViewResult.text = savedInstanceState.getString(RESULT)
+            findViewById<TextView>(R.id.textViewResult).text = savedInstanceState.getString(RESULT)
 
         findViewById<Button>(R.id.buttonResult).setOnClickListener {
-            val operation = textEditOperation.text.toString()
-            val expression = textEditExpression.text.toString()
 
-            publicNewton.getNewton(operation, expression).enqueue(object: Callback<Newton> {
+            publicNewton.getNewton(
+                findViewById<EditText>(R.id.operation).text.toString(),
+                findViewById<EditText>(R.id.expression).text.toString()
+            ).enqueue(object : Callback<Newton> {
 
                 @SuppressLint("SetTextI18n")
                 override fun onResponse(call: Call<Newton>, response: Response<Newton>) {
                     val resultNewton = response.body()
-                    if (resultNewton != null)
-                        textViewResult.text = getString(R.string.resultTextView) + " " + resultNewton.result.toString()
-                    else Toast.makeText(this@MainActivity, getString(R.string.error_result), Toast.LENGTH_SHORT).show()
+                    if (resultNewton != null) {
+                        GlobalScope.launch(Dispatchers.IO) {
+                            newtonDao.dropAll()
+                            newtonDao.insertAll(NewtonRoom(resultNewton.result.toString()))
+                        }
+                        findViewById<TextView>(R.id.textViewResult).text =
+                            getString(R.string.resultTextView) + " " + resultNewton.result.toString()
+                    } else Toast.makeText(
+                        this@MainActivity,
+                        getString(R.string.error_result),
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
 
                 override fun onFailure(call: Call<Newton>, t: Throwable) {
-                    Toast.makeText(this@MainActivity, getString(R.string.error_input_result), Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this@MainActivity,
+                        getString(R.string.error_input_result),
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             })
         }
+
         findViewById<Button>(R.id.buttonInformation).setOnClickListener {
             startActivity(Intent(this, UsageInformationActivity::class.java))
         }
     }
+
     companion object {
         const val RESULT = "RESULT"
     }
